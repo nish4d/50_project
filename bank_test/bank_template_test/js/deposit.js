f318377a2967efadaf343e5f23fa687a18dc16d3
nishad <nishadisl4m@gmail.com>
2023-02-13 17:24:13 +0600

    add validation

--- bank_test/bank_template_test/js/deposit.js
+++ bank_test/bank_template_test/js/deposit.js
@@ -1,10 +1,14 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){
     const depositField = takeInput('deposit-field');
+    if(isNaN(depositField)){
+        alert('Please enter a valid number');
+        return;
+    }
     const depositAmount = takeAmount('deposit-amount');
     const totalAmount = depositAmount + depositField;
     // alert(totalAmount)
-    paseValue('deposit-amount', totalAmount);
     const balanceAmount = takeAmount('balance-amount');
     const totalBalance = balanceAmount + depositField;
+    paseValue('deposit-amount', totalAmount);
     paseValue('balance-amount', totalBalance);
 })
